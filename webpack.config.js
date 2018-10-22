e2544fc342bffcad3510ea5869bdf58bebdd6a4c
Villi <vilhjalmur17@ru.is>
2018-10-22 00:11:58 +0000

    eitthvad rugl med github hann er down

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 //const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
+
 let pathsToClean = []
 module.exports = {
 	entry: { main: './src/client/index.js',
